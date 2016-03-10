d712f23dd5ff4fbdccddb0194509f982ef4e5eae
Hendrik Araujo Mourão <jhendrik@gmail.com>
2016-03-10 19:53:05 -0300

    jsx

--- script.jsx
+++ script.jsx
@@ -1,10 +1,22 @@
+var Card = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <img src="https://avatars.githubusercontent.com/u/6891916?v=3" alt="">
+        <h3>Hendrik Araujo</h3>
+        <hr/>
+      </div>
+    );
+  }
+});
+
 var Main = React.createClass({
   render: function() {
     return (
       <div>
-      
+        <Card /> 
       </div>
-    )
+    );
   }
 });
 
